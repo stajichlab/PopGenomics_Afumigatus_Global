b7d134a67e7a49a0f37e96c79f7260922a10955b
Jason Stajich <jasonstajich.phd@gmail.com>
2021-02-14 15:58:18 -0800

    sort the strain table

--- scripts/sra_table_to_samples.py
+++ scripts/sra_table_to_samples.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 import csv,sys,re
 outdir="SRA"
-outcsv = csv.writer(sys.stdout,delimiter=",")
-with open("lib/SRA_samples.csv","rt") as inp:
+with open("lib/SRA_samples.csv","rt") as inp, open("samples.csv","rt") as existing, open("samples.csv.2","w",newline="",encoding="utf-8") as outf:
+    outcsv = csv.writer(outf,delimiter=",")
     csvin = csv.reader(inp,delimiter=",")
+    sampin = csv.reader(existing,delimiter=",")
     table = {}
+    for row in sampin:
+        if row[0].startswith("Strain"):
+            continue
+        table[row[0]] = set(row[1].split(";"))
+
     for row in csvin:
         if row[0].startswith("RunAcc"):
             continue
         strain = row[1]
         strain = re.sub(r' ','_',strain)
+        strain = re.sub(r'\/','-',strain)
         srr    = row[0]
         srr    = "{}/{}_[12].fastq.gz".format(outdir,srr)
-        if strain in table:
-            table[strain].append(srr)
-        else:
-            table[strain] = [srr]
 
-for d in sorted(table):
-    outcsv.writerow([d,";".join(table[d])])
+        if strain in table:
+            table[strain].add(srr)
+        else:
+            table[strain] = set([srr])
+
+    inp.close()
+    existing.close()
+
+    for d in sorted(table,key=lambda strain: ((next(iter(table[strain]))).split('/')[0],strain)  ):
+        outcsv.writerow([d,";".join(sorted(table[d]))])
