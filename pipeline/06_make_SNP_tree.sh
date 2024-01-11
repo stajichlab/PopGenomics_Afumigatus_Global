f99dcd83b8479b953800413108d4d589cdf36e75
Jason Stajich <jasonstajich.phd@gmail.com>
2024-01-11 12:30:24 -0800

    updated to support conensus SNP calling

--- pipeline/06_make_SNP_tree.sh
+++ pipeline/06_make_SNP_tree.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-#SBATCH --mem=24gb --ntasks 48 --nodes 1 -C ryzen
+#SBATCH --mem=24gb --ntasks 24 --nodes 1
 #SBATCH --time=2:00:00 -p short
 #SBATCH -J maketree --out logs/make_tree.log
 
 module load yq
-module load workspace/scratch
 
 CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
@@ -27,22 +26,22 @@ if [[ -z $POPYAML || ! -s $POPYAML ]]; then
   exit
 fi
 
-module unload miniconda3
 module load parallel
 module load bcftools
 module load samtools
 module load iqtree
 module load fasttree
+module load workspace/scratch
 
 print_fas() {
-  printf ">%s\n%s\n" $1 $(bcftools query -s $1 -f '[%TGT]' $2)
+  printf ">%s\n%s\n" $1 $(bcftools query -s $1 -f '[%IUPACGT]' $2 | tr -d '\n')
 }
 
 iqtreerun() {
 	in=$1
 	out=$in.treefile
 	if [[ ! -f $out || $in -nt $out ]]; then
-		sbatch -p intel -n 6 -N 1 --mem 16gb -J iqtree --wrap "module load iqtree/2.2.1; iqtree2 -m GTR+ASC -s $in -nt AUTO -bb 1000 -alrt 1000"
+		sbatch -p intel -n 6 -N 1 --mem 16gb -J iqtree --wrap "module load IQ-TREE/2.2.2.6; iqtree2 -m GTR+ASC -s $in -st DNA -nt AUTO -bb 1000 -alrt 1000"
 	fi
 }
 
@@ -70,16 +69,12 @@ do
 
     vcf=$root.vcf.gz
     if [[ ! -f $FAS || ${vcf} -nt $FAS ]]; then
-      rm -f $FAS
-      vcftmp=$SCRATCH/$PREFIX.$POPNAME.$TYPE.combined_selected.bcf
-      bcftools view --threads 4 -e 'QUAL < 1000 || AF=1 || INFO/AF < 0.1' $vcf -Ob -o $vcftmp 
-      bcftools index $vcftmp
-
-      #rsync -a $vcf.tbi $vcftmp.tbi
+      vcftemp=$SCRATCH/$PREFIX.$POPNAME.$TYPE.vcf.gz
+      bcftools filter --threads $CPU  -Oz -o $vcftemp --SnpGap 3 -e 'QUAL < 1000 || AF=1 || INFO/AF < 0.05 || F_MISSING > 0' $vcf
+      bcftools index $vcftemp
       # no ref genome alleles
-      printf ">%s\n%s\n" $REFNAME $(bcftools query -f '%REF' ${vcftmp}) > $FAS
-      parallel -j $CPU print_fas ::: $(bcftools query -l ${vcf}) ::: $vcftmp >> $FAS
-      perl -ip -e 'if(/^>/){s/[\(\)#]/_/g; s/_+/_/g } else {s/[\*.]/-/g }' $FAS
+      printf ">%s\n%s\n" $REFNAME $(bcftools query -f '%REF' $vcftemp | tr -d '\n') > $FAS
+      parallel -j $CPU print_fas ::: $(bcftools query -l ${vcf}) ::: $vcftemp >> $FAS
     fi
   done
 done
